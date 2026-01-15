f18576bd7ad0a93563b694a3a10305d7784b7c5b
ang3lCerda <cerda086@gmail.com>
2026-01-15 00:01:55 -0600

    added end point to get a specific disc and weapon

--- app/app.py
+++ app/app.py
@@ -1,25 +1,56 @@
-from fastapi import FastAPI
-from app.db import drive_discs_collection, weapons_collection # your Motor collectio
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from app.db import drive_discs_collection, weapons_collection
 
 app = FastAPI()
 
-@app.get("/drive-discs")
-async def get_drive_discs():
-    # Exclude MongoDB _id
-    discs_list = await drive_discs_collection.find({}, {"_id": 0}).to_list(length=None)
-    
-    # Wrap by your custom Id
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/disc")
+async def get_all_drive_discs():
+    discs_list = await drive_discs_collection.find(
+        {},
+        {"_id": 0, "Story": 0}
+    ).to_list(length=None)
+
     wrapped = {str(disc["Id"]): disc for disc in discs_list}
-    
     return wrapped
+
+@app.get("/disc/{id}")
+async def get_disc(id:int):
+    disc = await drive_discs_collection.find_one({"Id": id})
+    
+    if not disc:
+        raise HTTPException(status_code=404, detail="Disc not found")
+    
+    disc["_id"] = str(disc["_id"])
+
+    return disc
 
 @app.get("/weapons")
-async def get_weapons():
-    # Exclude MongoDB _id
-    discs_list = await weapons_collection.find({}, {"_id": 0}).to_list(length=None)
-    
-    # Wrap by your custom Id
-    wrapped = {str(disc["Id"]): disc for disc in discs_list}
-    
+async def get_all_weapons():
+    weapons_list = await weapons_collection.find({}, {"_id": 0}).to_list(length=None)
+
+    wrapped = {str(weapon["Id"]): weapon for weapon in weapons_list}
     return wrapped
 
+@app.get("/weapon/{id}")
+async def get_weapon(id: int):
+    weapon = await weapons_collection.find_one({"Id": id})
+
+    if not weapon:
+        raise HTTPException(status_code=404, detail="Weapon not found")
+
+    # Optional: remove MongoDB _id entirely
+    weapon["_id"] = str(weapon["_id"])
+
+    return weapon
